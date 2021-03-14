c157e84dc0f9d993e0a903f142c3e3c9174ab877
korshunov80@mail.ru <korshunov80@mail.ru>
2021-03-14 14:56:01 +0300

    Update HomeWork_21_1.cpp

--- HomeWork_21_1.cpp
+++ HomeWork_21_1.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <vector>
 
-const int arrSize = 5;
-
 void swapvec (int* a, std::vector<int>& vec) {
-
-  int temp = 0;
-  for (int i = 0; i < arrSize; i++){
-    temp = *(a + i);
+  for (int i = 0; i < vec.size(); i++){
+    int temp = *(a + i);
     *(a + i) = vec[i];
     vec[i] = temp;
   }
@@ -15,11 +11,9 @@ void swapvec (int* a, std::vector<int>& vec) {
 
 int main() {
   std::vector<int> vect = {10, 15, 20, 25, 30};
-  int a[arrSize] = {1, 2, 3, 4, 5};
-
+  int a[5] = {1, 2, 3, 4, 5};
   swapvec(a, vect);
-
-  for (int i = 0; i < arrSize; i++){
+  for (int i = 0; i < vect.size(); i++){
   std::cout << vect[i] << " " << a[i] << std::endl;
   }
 }  
